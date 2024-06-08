8b07980f85e95e0363c13e19513affe5f71cc1ba
Cmd_ <uchzhezherov@gmail.com>
2024-06-08 15:32:40 +0300

    start

---
+++ src/main/java/com/ms8materials/Ms8Materials/Ms8MaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.ms8materials.Ms8Materials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms8MaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms8MaterialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms8materials/Ms8Materials/Ms8MaterialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms8materials.Ms8Materials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms8MaterialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
