f5a6cd0da01a776cc817688701a4bfa28e2a137d
Cmd_ <uchzhezherov@gmail.com>
2024-06-09 17:29:19 +0300

    bot config and /start handle

--- src/main/java/com/ms8materials/Ms8Materials/Ms8MaterialsApplication.java
+++ src/main/java/com/ms8materials/Ms8Materials/Ms8MaterialsApplication.java
@@ -1,13 +1,30 @@
 package com.ms8materials.Ms8Materials;
 
+import com.ms8materials.Ms8Materials.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
+@Slf4j
 public class Ms8MaterialsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ms8MaterialsApplication.class, args);
+	public static void main(String[] args) throws NoSuchFieldException {
+		ApplicationContext ctx = SpringApplication.run(Ms8MaterialsApplication.class, args);
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot((LongPollingBot) ctx.getBean("tgBotMain"));
+		} catch (TelegramApiException e) {
+			log.error(e.getMessage());
+		}
 	}
 
 }
