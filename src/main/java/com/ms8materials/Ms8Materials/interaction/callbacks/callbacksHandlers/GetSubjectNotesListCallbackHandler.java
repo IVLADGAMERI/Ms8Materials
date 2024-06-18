6e7e82309f8df8f0b40401fb89b95a93eabb175d
Cmd_ <uchzhezherov@gmail.com>
2024-06-18 20:03:06 +0300

    some callbacks handlers and refactoring

---
+++ src/main/java/com/ms8materials/Ms8Materials/interaction/callbacks/callbacksHandlers/GetSubjectNotesListCallbackHandler.java
@@ -0,0 +1,11 @@
+package com.ms8materials.Ms8Materials.interaction.callbacks.callbacksHandlers;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class GetSubjectNotesListCallbackHandler implements CallbackHandler{
+    @Override
+    public SendMessage handle(Update update) {
+        return null;
+    }
+}
