735e4e6bd0dd229096bfbb22b007e2c018556102
Cmd_ <uchzhezherov@gmail.com>
2024-06-18 20:09:56 +0300

    some callbacks handlers and refactoring

--- src/main/java/com/ms8materials/Ms8Materials/interaction/callbacks/CallbacksHandler.java
+++ src/main/java/com/ms8materials/Ms8Materials/interaction/callbacks/CallbacksHandler.java
@@ -1,9 +1,7 @@
 package com.ms8materials.Ms8Materials.interaction.callbacks;
 
 import com.ms8materials.Ms8Materials.essentials.CallbackType;
-import com.ms8materials.Ms8Materials.interaction.callbacks.callbacksHandlers.CallbackHandler;
-import com.ms8materials.Ms8Materials.interaction.callbacks.callbacksHandlers.GetSubjectFilesListCallbackHandler;
-import com.ms8materials.Ms8Materials.interaction.callbacks.callbacksHandlers.GetSubjectsListCallbackHandler;
+import com.ms8materials.Ms8Materials.interaction.callbacks.callbacksHandlers.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -16,10 +14,22 @@ public class CallbacksHandler {
     private final Map<CallbackType, CallbackHandler> callbacksHandlers;
 
     public CallbacksHandler(@Autowired GetSubjectsListCallbackHandler getSubjectsListCallbackHandler,
-                            @Autowired GetSubjectFilesListCallbackHandler getSubjectFilesListCallbackHandler) {
+                            @Autowired GetSubjectFilesListCallbackHandler getSubjectFilesListCallbackHandler,
+                            @Autowired GetSubjectNotesListCallbackHandler getSubjectNotesListCallbackHandler,
+                            @Autowired GetSubjectPhotosListCallbackHandler getSubjectPhotosListCallbackHandler,
+                            @Autowired GetSubjectMaterialsCallbackHandler getSubjectMaterialsCallbackHandler,
+                            @Autowired GetSubjectFileCallbackHandler getSubjectFileCallbackHandler,
+                            @Autowired GetSubjectPhotoCallbackHandler getSubjectPhotoCallbackHandler,
+                            @Autowired GetSubjectNoteCallbackHandler getSubjectNoteCallbackHandler) {
         this.callbacksHandlers = Map.of(
                 CallbackType.GET_SUBJECTS_LIST, getSubjectsListCallbackHandler,
-                CallbackType.GET_SUBJECT_FILES_LIST, getSubjectFilesListCallbackHandler);
+                CallbackType.GET_SUBJECT_FILES_LIST, getSubjectFilesListCallbackHandler,
+                CallbackType.GET_SUBJECT_NOTES_LIST, getSubjectNotesListCallbackHandler,
+                CallbackType.GET_SUBJECT_PHOTOS_LIST, getSubjectPhotosListCallbackHandler,
+                CallbackType.GET_SUBJECT_MATERIALS, getSubjectMaterialsCallbackHandler,
+                CallbackType.GET_SUBJECT_FILE, getSubjectFileCallbackHandler,
+                CallbackType.GET_SUBJECT_PHOTO, getSubjectPhotoCallbackHandler,
+                CallbackType.GET_SUBJECT_NOTE, getSubjectNoteCallbackHandler);
     }
 
     public SendMessage handle(Update update) {
--- src/main/java/com/ms8materials/Ms8Materials/interaction/callbacks/callbacksHandlers/GetSubjectFileCallbackHandler.java
+++ src/main/java/com/ms8materials/Ms8Materials/interaction/callbacks/callbacksHandlers/GetSubjectFileCallbackHandler.java
@@ -1,8 +1,10 @@
 package com.ms8materials.Ms8Materials.interaction.callbacks.callbacksHandlers;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class GetSubjectFileCallbackHandler implements CallbackHandler{
     @Override
     public SendMessage handle(Update update) {
